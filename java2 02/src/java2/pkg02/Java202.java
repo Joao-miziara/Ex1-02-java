760cd0da29e58049aa120dc6347ce373d8abf1d8
Joao-miziara <65513502+Joao-miziara@users.noreply.github.com>
2020-06-22 21:59:47 -0300

    Add files via upload

---
+++ java2 02/src/java2/pkg02/Java202.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2.pkg02;
+
+/**
+ *
+ * @author jpmiz
+ */
+public class Java202 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
